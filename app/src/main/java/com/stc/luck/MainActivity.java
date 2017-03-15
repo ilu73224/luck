f5b410422bd09d283aa56e78592bf60f65edeb10
daniel.chiou <ilu73224@gmail.com>
2017-03-15 15:17:48 +0800

    remove log

--- app/src/main/java/com/stc/luck/MainActivity.java
+++ app/src/main/java/com/stc/luck/MainActivity.java
@@ -2,7 +2,6 @@ package com.stc.luck;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 
 import java.io.IOException;
 
@@ -11,13 +10,11 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
         try {
             p = Runtime.getRuntime().exec("su shell input keyevent 26");
         } catch (IOException e) {
             e.printStackTrace();
         }
-        Log.d("QQ", "dd");
-        finish();//
+        finish();
     }
 }
