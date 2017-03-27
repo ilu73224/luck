3a7c6fc8bf7d131fc41254942164a188078f28cc
daniel.chiou <ilu73224@gmail.com>
2017-03-27 11:42:02 +0800

    1.use asynctask to improve ui experience. 2.use su command correctly.

--- app/src/main/java/com/stc/luck/MainActivity.java
+++ app/src/main/java/com/stc/luck/MainActivity.java
@@ -1,20 +1,57 @@
 package com.stc.luck;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MainActivity extends Activity {
-    private Process p=null;
+    public static final String TAG = "luck";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        try {
-            p = Runtime.getRuntime().exec("su shell input keyevent 26");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        new mainTask().execute();
         finish();
+    }
+    private class mainTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                Process pSendPowerKeyEvent = Runtime.getRuntime().exec("su");
+                DataOutputStream opt = new DataOutputStream(pSendPowerKeyEvent.getOutputStream());
+                opt.writeBytes("input keyevent 26\n");
+                opt.writeBytes("exit\n");
+                opt.flush();
+                pSendPowerKeyEvent.waitFor();
+                if(0 == pSendPowerKeyEvent.exitValue()) {
+                    BufferedReader bufferedReader = new BufferedReader(
+                            new InputStreamReader(pSendPowerKeyEvent.getInputStream()));
+                    StringBuilder log = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        log.append(String.format("%s\n", line));
+                    }
+                    if(0 < log.length()) Log.d(TAG, "run success result = \n " + log.toString());
+                } else {
+                    Log.d(TAG, "run failed exit value =  " + pSendPowerKeyEvent.exitValue());
+                    BufferedReader bufferedReader = new BufferedReader(
+                            new InputStreamReader(pSendPowerKeyEvent.getErrorStream()));
+                    StringBuilder log = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        log.append(String.format("%s\n", line));
+                    }
+                    if(0 < log.length()) Log.d(TAG, "Error = \n " + log.toString());
+                }
+            } catch (InterruptedException | IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
